0dd813305a3f9286414db4877e806b9c64ddfede
DayaneLopes <dayanelopeslee@gmail.com>
2019-08-07 21:00:01 -0300

    Projeto criado

---
+++ src/main/java/br/com/meatapp/MeatApp2Application.java
@@ -0,0 +1,13 @@
+package br.com.meatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatApp2Application.class, args);
+	}
+
+}
